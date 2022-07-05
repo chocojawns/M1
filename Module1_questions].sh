b47753f0917b55fdf37d4d79248e752e0c64836a
chocojawns <andrew@johnson.vet>
2022-07-04 21:06:56 -0400

    updated questions

--- Module1_questions].sh
+++ Module1_questions].sh
@@ -3,15 +3,15 @@
 
 echo " 
 
-1. Which layer of the hierarchical LAN design model defines the summarization boundary for 
-    the network control plane protocols and serves as the policy boundary?
+1. Which layer of the hierarchical LAN design model defines the summarization
+boundary for the network control plane protocols and serves as the policy boundary?
 
 2. What is the primary motivation for deploying a two-tier (collapsed-core) design?
 
 3. With HSRP, in the case of a tie, what is used to determine the priority?
 
-4. Which hardware redundancy mechanism supports redundancy in a switch by allowing a redundant
-    supervisor engine to take over if the primary supervisor engine fails?
+4. Which hardware redundancy mechanism supports redundancy in a switch by allowing
+a redundant supervisor engine to take over if the primary supervisor engine fails?
 
     Answers
 1. Distribution layer
@@ -20,4 +20,4 @@ design is the cost reduction available along with most of the three-tier
 design benefits.
 3. In the case of a tie, the primary IP addresses are compared, and the
 device with the higher IP address has priority.
-4. Stateful Switchover (SSO) 
+4. Stateful Switchover (SSO)"
